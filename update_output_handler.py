d3ee6ce98a70670835ccd22d41c9f4651a4513e6
Dominik Ciołczyk <44479446+Dodek69@users.noreply.github.com>
2024-04-29 15:56:19 +0200

    Make pins private

--- update_output_handler.py
+++ update_output_handler.py
@@ -11,18 +11,18 @@ from interfaces.msg import CommandArray
 
 class UpdateOutputHandler:
     def __init__(self):
-        self.pins = [CommandArray.GO_FORWARD, CommandArray.GO_LEFT, CommandArray.GO_RIGHT, CommandArray.GO_BACKWARD]
+        self._pins = [CommandArray.GO_FORWARD, CommandArray.GO_LEFT, CommandArray.GO_RIGHT, CommandArray.GO_BACKWARD]
         GPIO.setmode(GPIO.BOARD)
-        for pin in self.pins:
+        for pin in self._pins:
             GPIO.setup(pin, GPIO.OUT)
         
-        GPIO.output(self.pins, [GPIO.LOW] * len(self.pins))
+        GPIO.output(self._pins, [GPIO.LOW] * len(self._pins))
         
     def update_output(self, command: int) -> None:
         """Update the output based on the given command. If the command is not in the list of pins, set all pins to LOW."""
-        pin_states = {pin: GPIO.LOW for pin in self.pins}
+        pin_states = {pin: GPIO.LOW for pin in self._pins}
         
-        if command in self.pins:
+        if command in self._pins:
             pin_states[command] = GPIO.HIGH
         elif command != CommandArray.STOP:
             print(f"Invalid command: {command}")
